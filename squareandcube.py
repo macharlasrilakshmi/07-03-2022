b25ef4f10caede4a96f0b716a972c4728fa29509
macharlasrilakshmi <101548823+macharlasrilakshmi@users.noreply.github.com>
2022-03-15 16:49:33 +0530

    Create squareandcube.py

---
+++ squareandcube.py
@@ -0,0 +1,5 @@
+y=[int(i) for i in input("Enter the numbers")]
+NumSquare = list(map(lambda i: i**2, y))
+NumCube = list(map(lambda i: i**3, y))
+print(" the Square of the  numbers=", NumSquare)
+print(" the Cube of  the numbers= ", NumCube)
